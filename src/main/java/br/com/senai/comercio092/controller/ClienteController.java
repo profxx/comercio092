08715eee040c35d2ec8c5f653c7e1cc5e21ffd84
Alexandre <acpimentel@gmail.com>
2024-05-13 11:28:43 -0300

    First commit

---
+++ src/main/java/br/com/senai/comercio092/controller/ClienteController.java
@@ -0,0 +1,47 @@
+package br.com.senai.comercio092.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.senai.comercio092.entity.Cliente;
+import br.com.senai.comercio092.service.ClienteService;
+
+@RestController
+@RequestMapping("/cliente")
+public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Cliente>> findAll(){
+        List<Cliente> clientes = clienteService.findAll();
+        return ResponseEntity.ok().body(clientes);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> findById(@PathVariable Long id){
+        Cliente cliente = clienteService.findById(id);
+        return ResponseEntity.ok().body(cliente);
+    }
+    @PostMapping("/inserir")
+    public ResponseEntity<Cliente> insertNew(@RequestBody Cliente cliente){
+        Cliente clienteInserido = clienteService.insertNew(cliente);
+        return ResponseEntity.ok().body(clienteInserido);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> update(@PathVariable Long id, @RequestBody Cliente cliente){
+        Cliente clienteAtualizado = clienteService.update(id, cliente);
+        return ResponseEntity.ok().body(clienteAtualizado);
+    }
+    
+
+}
